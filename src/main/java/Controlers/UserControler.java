7c5f2987c393504ea1b50e17280df7c3a75f3c31
Tomasz Lucki <lucki@email.com>
2018-04-21 10:34:13 +0200

    2 - dodany user

---
+++ src/main/java/Controlers/UserControler.java
@@ -0,0 +1,11 @@
+package Controlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserControler {
+
+    private List usersList = new ArrayList();
+
+    
+}
--- src/main/java/Mian.java
+++ src/main/java/Mian.java
@@ -1,5 +1,6 @@
 public class Mian {
     public static void main(String[] args) {
-        
+
+
     }
 }
