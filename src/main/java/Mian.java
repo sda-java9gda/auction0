2f92c40c137a77cdc54e7522ea8b7164b4fff244
Lukasz Nowocinski/Nowat <nowat@email.com>
2018-04-21 14:20:24 +0200

    Usuniecie

--- src/main/java/Mian.java
+++ src/main/java/Mian.java
@@ -4,12 +4,7 @@ public class Mian {
     public static void main(String[] args) {
 
         UserControler userControler = new UserControler();
-
-        userControler.createUser("Lukasz","1234");
-        userControler.createUser("Tomek","23");
-        userControler.createUser("Mateusz","5555");
-        userControler.createUser("Asia","11111");
-
+        
 
     }
 }
