dfd341252c7cf9eea684f52241acf4601163791c
Lukasz Nowocinski/Nowat <nowat@email.com>
2018-04-21 11:08:31 +0200

    commit add

--- src/main/java/controlers/UserControler.java
+++ src/main/java/controlers/UserControler.java
@@ -1,11 +1,21 @@
 package controlers;
 
+import models.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserControler {
 
-    private List usersList = new ArrayList();
+    private List<User> usersList = new ArrayList<User>();
+
+    public void addUser (User user){
+        usersList.add(user);
+//        if (usersList.contains(user)){
+//            System.out.println("Login zajęty");
+        }
+    }
 
 
-}
+
+
