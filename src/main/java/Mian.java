a573f3b33170529e86895cf99bcd30fb42a70c88
Lukasz Nowocinski/Nowat <nowat@email.com>
2018-04-22 10:22:27 +0200

    daasdas

--- src/main/java/Mian.java
+++ src/main/java/Mian.java
@@ -1,10 +1,10 @@
 import controlers.UserControler;
 
+
 public class Mian {
     public static void main(String[] args) {
 
         UserControler userControler = new UserControler();
-        
 
     }
 }
