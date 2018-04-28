225537fe1bc6fe3b9e211466c1c0968c19536d2f
Tomasz Lucki <lucki@email.com>
2018-04-28 14:52:33 +0200

    login register + tests ok

--- src/test/java/UserControllerTest.java
+++ src/test/java/UserControllerTest.java
@@ -1,4 +1,5 @@
 import controllers.UserController;
+import controllers.UserFileController;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import models.User;
@@ -6,10 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.assertj.core.api.Assertions.*;
 
 @RunWith(JUnitParamsRunner.class)
@@ -17,6 +23,7 @@ public class UserControllerTest {
 
     private UserController uc;
     private Map<String, User> usersMap;
+    private String filePath;
 
     public Object paramsForTrue() {
         return new Object[][]{
@@ -37,15 +44,15 @@ public class UserControllerTest {
     }
 
     @Before
-    public void createObjects(){
+    public void setUp() {
         uc = new UserController();
         usersMap = new HashMap<>();
+        filePath="src/main/resources/TestUsersDataBase.txt";
     }
 
     @Test
     @Parameters(method = "paramsForTrue")
-    public void followParamsForIsUserExistShouldBeTrue(User user, boolean expected) {
-        createObjects();
+    public void followParamsForIsUserExistShouldBeTrue(User user, boolean expected) throws Exception{
         //given
         //user, usersMap
         //when
@@ -57,7 +64,7 @@ public class UserControllerTest {
 
     @Test
     @Parameters(method = "paramsForFalse")
-    public void isUserExistShouldBeFalse(User user, boolean expected) {
+    public void isUserExistShouldBeFalse(User user, boolean expected)throws Exception {
         //given
         //user, usersMap
         //when
@@ -73,64 +80,104 @@ public class UserControllerTest {
         //given
         //user, usersMap
         //when
-        boolean actual = uc.addUser(user.getLogin(),user.getPassword(),usersMap);
+        boolean actual = uc.addUser(user.getLogin(), user.getPassword(), usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
+
     @Test
     @Parameters(method = "paramsForFalse")
-    public void addUserShouldBeFalse(User user, boolean expected) {
+    public void addUserShouldBeFalse(User user, boolean expected) throws Exception{
         //given
         //user
-        usersMap.put(user.getLogin(),user);
+        usersMap.put(user.getLogin(), user);
         //when
-        boolean actual = uc.addUser(user.getLogin(),user.getPassword(),usersMap);
+        boolean actual = uc.addUser(user.getLogin(), user.getPassword(), usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @Parameters(method = "paramsForTrue")
-    public void removeUserShouldBeTrue(User user, boolean expected){
+    public void removeUserShouldBeTrue(User user, boolean expected) throws Exception{
         //given
         //user
-        usersMap.put(user.getLogin(),user);
+        usersMap.put(user.getLogin(), user);
         //when
-        boolean actual = uc.removeUser(user,usersMap);
+        boolean actual = uc.removeUser(user, usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
+
     @Test
     @Parameters(method = "paramsForFalse")
-    public void removeUserShouldBeFalse(User user, boolean expected){
+    public void removeUserShouldBeFalse(User user, boolean expected) throws Exception{
         //given
         //user, userMap
         //when
-        boolean actual = uc.removeUser(user,usersMap);
+        boolean actual = uc.removeUser(user, usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
+
     @Test
     @Parameters(method = "paramsForTrue")
-    public void logInUserShouldBeTrue(User user, boolean expected){
+    public void logInUserShouldBeTrue(User user, boolean expected)throws Exception {
         //given
         //user
-        usersMap.put(user.getLogin(),user);
+        usersMap.put(user.getLogin(), user);
         //when
-        boolean actual = uc.logInUser(user.getLogin(),user.getPassword(),usersMap);
+        boolean actual = uc.logInUser(user.getLogin(), user.getPassword(), usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
+
     @Test
     @Parameters(method = "paramsForFalse")
-    public void logInUserShouldBeFalse(User user, boolean expected){
+    public void logInUserShouldBeFalse(User user, boolean expected)throws Exception {
         //given
         //user, userMap
         //when
-        boolean actual = uc.logInUser(user.getLogin(),user.getPassword(),usersMap);
+        boolean actual = uc.logInUser(user.getLogin(), user.getPassword(), usersMap);
         //then
         assertThat(actual).isEqualTo(expected);
     }
-    //Napisac testy dla getMapOfUsers i saveUsersMapToFile
+
+//    @Test
+//    @Parameters(method = "paramsForTrue")
+//    public void saveUsersMapToFileShouldBeTrue(boolean expected){
+//        //given
+//        //userMap
+//        //when
+//        usersMap.put("tom",new User("tom","1233"));
+//        boolean actual = UserFileController.writeUsersToDataBaseFile(usersMap, filePath);
+//        //then
+//        assertThat(actual).isEqualTo(expected);
+//
+//    }
+
+    @Test
+    public void getMapOfUsersShouldReturnMapThatContainsKey() throws Exception {
+        //given
+        Files.write(Paths.get(filePath), "aa;;aa;;bb\n".getBytes(), StandardOpenOption.CREATE);
+        //nio
+        //when
+        Map<String, User> actual = uc.getMapOfUsers(filePath);
+
+        //then
+        assertTrue(actual.containsKey("aa"));
+    }
+
+    @Test
+    public void getMapOfUsersShouldReturnMapThatContainsUser() throws Exception{
+        //given
+        Files.write(Paths.get(filePath),"tom;;tom;;1234\n".getBytes(),StandardOpenOption.CREATE);
+        User user = new User("tom","1234");
+        //when
+        Map<String,User> actual = uc.getMapOfUsers(filePath);
+        actual.put(user.getLogin(),user);
+        //then
+        assertTrue(actual.containsValue(user));
+    }
 
 }
